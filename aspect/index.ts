4046b12e800fcddeb0e23e026773aa6ec04e4d3d
zhoubin6 <zhoubin6@tal.com>
2024-07-12 10:31:49 +0800

    feat: 限流器代码补全

--- aspect/index.ts
+++ aspect/index.ts
@@ -20,18 +20,46 @@ class Aspect implements IPreContractCallJP {
      */
     preContractCall(input: PreContractCallInput): void {
         // read the throttle config from the properties and decode
+        // 读取限流配置
+        const interval = sys.aspect.property.get<u64>("interval"); 
+        // 次数
+        const limit = sys.aspect.property.get<u64>("limit");
 
         // get the contract address, from address and build the storage prefix
+        // 获取当前调用的合约地址
+        const contranctAddress = uint8ArrayToHex(input.call!.to);
+        // 发送地址
+        const from = uint8ArrayToHex(input.call!.from);
+        const storagePrefix = `${contranctAddress}:${from}`;
 
         // load the current block timestamp
+        // 获取区块的时间戳,通过底层hostApi获取
+        const blockTimeBytes = sys.hostApi.runtimeContext.get<u64>('block.header.timestamp');
+        console.log('blockTimeBytes', blockTimeBytes);
+        const blockTimes = Protobuf.decode<UintData>(blockTimeBytes, UintData.decode).data;
+        console.log('blockTimes', blockTimes);
 
         // load last execution timestamp
+         // 获取最近一次区块的交易,根据获取的合约地址和交易地址组成的key，通过aspect本身存储的数据获取
+         const lastExecState = sys.aspect.mutableState.get<u64>(storagePrefix + 'lastExecAt');
+         console.log(lastExecState, 'lastExecState');
+         const lastExec = lastExecState.unwrap();
 
         // check if the throttle interval has passed, revert if not
-
+        // 交易回滚
+        if (lastExec > 0 && (blockTimes - lastExec) < interval) {
+            sys.revert('throttled');
+        }
         // check if the throttle limit has been reached, revert if so
-
+        // 检查执行次数
+        const execTimeState = sys.aspect.mutableState.get<u64>(storagePrefix + 'execTimes');
+        const execTimes = execTimeState.unwrap();
+        if (limit && execTimes > limit) {
+            sys.revert('exec times exceeded');
+        }
         // update the throttle state
+        execTimeState.set(execTimes + 1);
+        lastExecState.set(blockTimes);
     }
 
     /**
